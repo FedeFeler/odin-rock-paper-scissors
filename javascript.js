8dc579290ef9c21cfdc4e3458d5b93337d23b0b2
Federico <fledesmarubulotta@gmail.com>
2025-08-22 13:21:31 -0300

    Add scoring and winner sistem

--- javascript.js
+++ javascript.js
@@ -14,40 +14,54 @@ function getHumanChoice() {
     return humanChoice
 }
 
-
+let humanScore = 0;
+let computerScore = 0;
 
 function playGame() {
 
-    let humanScore = 0;
-    let computerScore = 0;
+    
+    
 
     function playRound(humanChoice, computerChoice) {
+    let winner;
     
     //Human wins
 
     if (humanChoice === "paper" && computerChoice === "rock") {
-        alert("You win! Paper beats rock.")
+        alert("You win! Paper beats rock.");
+        winner = "human";
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        alert("You win! Scissors beats paper.")
+        alert("You win! Scissors beats paper.");
+        winner = "human";
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        alert("You win! Rock beats scissors.")
+        alert("You win! Rock beats scissors.");
+        winner = "human";
     } 
     
     // Human loses
 
     else if (humanChoice === "paper" && computerChoice === "scissors") {
-        alert("You lose! Scissors beats paper.")
+        alert("You lose! Scissors beats paper.");
+        winner = "computer";
     } else if (humanChoice === "rock" && computerChoice === "paper") {
-        alert("You lose! Paper beats rock.")
+        alert("You lose! Paper beats rock.");
+        winner = "computer";
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        alert("You lose! Rock beats scissors.")
+        alert("You lose! Rock beats scissors.");
+        winner = "computer";
     } 
     
     // It's a tie
 
     else if (humanChoice === computerChoice) {
-        alert("It's a tie!")
+        alert("It's a tie!");
+        winner = "tie";
     } 
+    if (winner === "human") {
+        humanScore += 1;
+    } else if (winner === "computer") {
+        computerScore += 1;
+    }
 }
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
@@ -60,3 +74,12 @@ while (roundCounter <= 4) {
     playGame();
     roundCounter += 1;
 } 
+
+if (humanScore > computerScore) {
+    alert(`Human score: ${humanScore}.\nComputer score: ${computerScore}.\nWinner: Human.`)
+} else if (humanScore < computerScore) {
+    alert(`Human score: ${humanScore}.\nComputer score: ${computerScore}.\n
+Winner: Computer.`)
+} else {
+    alert("It's a tie.")
+}
